cce40c1646c299166cc20e17354f96c22231ca6b
jrchan01 <jrchan01@student.ubc.ca>
2024-11-06 17:01:15 -0800

    Rough draft

--- Gapminder/app.R
+++ Gapminder/app.R
@@ -28,9 +28,9 @@ ui <- fluidPage(
         tabPanel("Table", DTOutput("table")),
         tabPanel("Plot", plotOutput("plot")),
         tabPanel("World Map", 
-                 tags$img(src = "https://upload.wikimedia.org/wikipedia/commons/2/2d/Continental_models-Australia.gif", 
-                          alt = "World Map with Continents", 
-                          width = "100%"))
+                 img(src = "world_map.png", 
+                     alt = "World Map with Continents", 
+                     width = "100%"))
       )
     )
   )
@@ -41,33 +41,5 @@ server <- function(input, output) {
   # Reactive expression to filter data
   filtered_data <- reactive({
     data <- gapminder
-    if (length(input$continent) > 0) {
-      data <- data[data$continent %in% input$continent, ]
-    }
-    data <- data[data$year >= input$year[1] & data$year <= input$year[2], ]
-    data
-  })
-  
-  # Output the filtered table
-  output$table <- renderDT({
-    datatable(filtered_data(), filter = 'top', options = list(pageLength = 10))
-  })
-  
-  # Output the number of results
-  output$result_count <- renderText({
-    paste("Number of results:", nrow(filtered_data()))
-  })
-  
-  # Output the plot
-  output$plot <- renderPlot({
-    ggplot(filtered_data(), aes(x = year, y = lifeExp, color = continent)) +
-      geom_line() +
-      labs(title = "Life Expectancy Over Time",
-           x = "Year",
-           y = "Life Expectancy") +
-      theme_minimal()
-  })
-}
-
-# Run the app
-shinyApp(ui = ui, server = server)
+    if
+    
